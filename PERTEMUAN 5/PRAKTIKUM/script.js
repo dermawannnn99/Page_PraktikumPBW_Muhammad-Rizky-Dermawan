79e6eda1ed39bf31b172b4f30edcdcc6b027cf22
dermawannnn99 <rizkydermawan12345@gmail.com>
2026-03-09 11:00:19 +0700

    first

---
+++ PERTEMUAN 5/PRAKTIKUM/script.js
@@ -0,0 +1,13 @@
+        function salam() {
+            alert("Selamat Datang di Website Kami")
+        }
+
+        function contohLet() {
+            let y = 10;
+
+            if (true) {
+                let y = 20;
+                console.log("Nilai Dalam Blok:", y);
+            }
+                console.log("Nilai Luar Blok", y);
+        }
